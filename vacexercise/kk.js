ce9d179e7ab3d2495339f7d92077755641822601
kavineshkavinx <kavineshsri258@gmail.com>
2023-07-25 13:25:55 +0530

    all

---
+++ vacexercise/kk.js
@@ -0,0 +1,19 @@
+//console
+console.log("heloo...");
+
+//comments
+/*
+cduchydncydbycydgc
+*/
+
+//variables--let
+let name = "kavinesaaaa....";
+console.log(name)
+name="258";
+console.log(name)
+//variables--const
+const movie=("KGF");
+console.log(movie)
+movie="kd";
+console.log(movie)
+//-----
